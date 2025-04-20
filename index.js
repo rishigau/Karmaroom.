b9b0d8f7b43cf86db2a1b43d88e4ae767895ce35
rishigau <xdream953@gmail.com>
2025-04-20 19:18:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import VideoChat from '../components/VideoChat'; // Placeholder video chat component
+import VideoChat from '../components/VideoChat';
 
 const HomePage = () => {
   return (
